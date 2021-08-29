f4b28ee31debf7e8db832ceab1dea1b07b172fa3
Bruno <bruno.smsouza@gmail.com>
2021-08-29 19:40:11 -0300

    Firts Commit

---
+++ Eggerp-api/src/main/java/com/eggerp/EggerpApiApplication.java
@@ -0,0 +1,13 @@
+package com.eggerp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EggerpApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EggerpApiApplication.class, args);
+	}
+
+}
---
+++ Eggerp-api/src/test/java/com/eggerp/EggerpApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eggerp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EggerpApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
