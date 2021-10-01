449c671e1900432ae69762ad6603ca3d032a9baa
Bruno <bruno.smsouza@gmail.com>
2021-10-01 19:03:52 -0300

    Retirando ChickenLineage e Shed de ChicknelLotidInput

--- Eggerp-api/src/main/java/com/eggerp/api/model/input/ChickenLotIdInput.java
+++ Eggerp-api/src/main/java/com/eggerp/api/model/input/ChickenLotIdInput.java
@@ -2,10 +2,6 @@ package com.eggerp.api.model.input;
 
 import javax.validation.constraints.NotNull;
 
-import com.eggerp.domain.model.ChickenLineage;
-import com.eggerp.domain.model.Shed;
-import com.fasterxml.jackson.annotation.JsonIgnore;
-
 import lombok.Getter;
 import lombok.Setter;
 
@@ -15,11 +11,5 @@ public class ChickenLotIdInput {
 
 	@NotNull
 	private Long id;
-	
-//	@JsonIgnore
-//	private ChickenLineage chickenLineage;
-//	
-//	@JsonIgnore
-//	private Shed shed;
 	
 }
